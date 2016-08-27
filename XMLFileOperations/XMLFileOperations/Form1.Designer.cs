4acf67182f47b60e13650576961e47a0c76005df
swornim nepal <swornim nepal>
2016-08-27 20:41:46 +0545

    Initial

---
+++ XMLFileOperations/XMLFileOperations/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace XMLFileOperations
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.jd = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(11, 113);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(438, 123);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.RowHeaderMouseDoubleClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_RowHeaderMouseDoubleClick);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(59, 78);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(64, 29);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(199, 78);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(64, 29);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(129, 78);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(64, 29);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // jd
+            // 
+            this.jd.AutoSize = true;
+            this.jd.Location = new System.Drawing.Point(56, 13);
+            this.jd.Name = "jd";
+            this.jd.Size = new System.Drawing.Size(35, 13);
+            this.jd.TabIndex = 4;
+            this.jd.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(59, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Email";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(97, 55);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(67, 17);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Is Active";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(97, 6);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(127, 20);
+            this.txtName.TabIndex = 8;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(97, 29);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(127, 20);
+            this.txtEmail.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(461, 239);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.jd);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Label jd;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtEmail;
+    }
+}
+
---
+++ XMLFileOperations/XMLFileOperations/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XMLFileOperations
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadGrid();
+        }
+
+        private void LoadGrid()
+        {
+            DataSet ds = new DataSet();
+            ds.ReadXml(Application.StartupPath + "\\Data.xml");
+            dataGridView1.DataSource = ds.Tables["email"];
+            btnSave.Text = "Save";
+            btnDelete.Enabled = false;
+        }
+        int rowid = 0;
+
+        private void dataGridView1_RowHeaderMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            rowid = dataGridView1.CurrentRow.Index;
+            btnSave.Text = "Update";
+            btnDelete.Enabled = true;
+            txtName.Text = dataGridView1.CurrentRow.Cells["Name"].Value.ToString();
+            txtEmail.Text = dataGridView1.CurrentRow.Cells["address"].Value.ToString();
+            string active = dataGridView1.CurrentRow.Cells["active"].Value.ToString();
+            if ( Convert.ToBoolean( active)==true)
+            {
+                checkBox1.Checked = true; 
+            }
+            else
+            {
+                checkBox1.Checked = false;
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Clear();
+            LoadGrid();
+        }
+
+        private void Clear()
+        {
+            txtName.Clear();
+            txtEmail.Clear();
+            checkBox1.Checked = false;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (btnSave.Text!="Update")
+            {
+
+                DataSet ds = new DataSet();
+                ds.ReadXml(Application.StartupPath + "\\Data.xml");
+                DataRow dr = ds.Tables["email"].NewRow();
+                dr[0] = txtName.Text;
+                dr[1] = txtEmail.Text;
+                dr[2] = checkBox1.Checked ? "true" : "false";
+                ds.Tables["email"].Rows.Add(dr);
+                ds.WriteXml(Application.StartupPath + "\\Data.xml");
+                LoadGrid();
+                Clear();
+                
+            }
+            else
+            {
+                DataSet ds = new DataSet();
+                ds.ReadXml(Application.StartupPath + "\\Data.xml");
+                ds.Tables["email"].Rows[rowid]["Name"] = txtName.Text;
+                ds.Tables["email"].Rows[rowid]["address"] = txtEmail.Text;
+                ds.Tables["email"].Rows[rowid]["active"] = checkBox1.Checked ? "True" : "False";
+                ds.AcceptChanges();
+                ds.WriteXml(Application.StartupPath + "\\Data.xml");
+                LoadGrid();
+               
+                 
+
+
+
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            DataSet ds = new DataSet();
+            ds.ReadXml(Application.StartupPath + "\\Data.xml");
+            ds.Tables["email"].Rows[rowid].Delete();
+            ds.AcceptChanges();
+            ds.WriteXml(Application.StartupPath + "\\Data.xml");
+            LoadGrid();
+        }
+    }
+}
